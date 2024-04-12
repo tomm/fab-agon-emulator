f9b2d89110031c49acb22f53e2210bb2d21c6438
Paul Gregory <pgregory@indigobeetle.co.uk>
2024-04-12 17:53:38 +0100

    Fix macOS distribution script.
     * Wasn't getting the executable from the correct place.

--- dist_scripts/make-dist-macos.sh
+++ dist_scripts/make-dist-macos.sh
@@ -7,7 +7,7 @@ DIST_DIR=fab-agon-emulator-$VERSION-macos
 
 rm -rf $DIST_DIR
 mkdir $DIST_DIR
-cp ./target/release/fab-agon-emulator $DIST_DIR
+cp ./fab-agon-emulator $DIST_DIR
 cp -r ./firmware $DIST_DIR
 cp LICENSE README.md $DIST_DIR
 mkdir $DIST_DIR/sdcard
